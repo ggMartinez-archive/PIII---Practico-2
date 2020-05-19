ce6a8fabcc7f59b6be9ffb7337e083edb7ba5457
Gonzalo Martinez <gon.martinez@icloud.com>
2020-05-19 19:22:34 -0300

    Primer Commit

---
+++ Practico2/Classes/Persona.vb
@@ -0,0 +1,73 @@
+﻿Public Class Persona
+
+    Private id As Integer
+    Private nombre As String
+    Private apellido As String
+    Private telefono As String
+    Private direccion As String
+
+    Public Sub SetId(idRecibido As String)
+        Me.id = idRecibido
+    End Sub
+
+    Public Sub SetNombre(nombreRecibido As String)
+        Me.nombre = nombreRecibido
+    End Sub
+
+    Public Sub SetApellido(apellidoRecibido As String)
+        Me.apellido = apellidoRecibido
+    End Sub
+
+    Public Sub SetTelefono(telefonoRecibido As String))
+        Me.telefono = telefonoRecibido
+    End Sub
+
+    Public Sub SetDireccion(direccionRecibida As String)
+        Me.direccion = direccionRecibida
+    End Sub
+
+    Public Function GetNombre()
+        Return Me.nombre
+    End Function
+
+    Public Function GetId()
+        Return Me.id
+    End Function
+
+    Public Function GetApellido()
+        Return Me.apellido
+    End Function
+
+    Public Function GetDireccion()
+        Return Me.direccion
+    End Function
+
+    Public Function GetTelefono()
+        Return Me.telefono
+    End Function
+
+    Public Function ValidarID()
+        ' Validar que sea numero, menor a 99
+
+    End Function
+
+    Public Function ValidaNombre()
+        ' Validar que tenga menos de 20 caracteres
+
+    End Function
+
+    Public Function ValidarApellido()
+        ' Validar que tenga menos de 20 caracteres
+
+    End Function
+
+    Public Function ValidarDireccion()
+        ' Validar que tenga menos de 50 caracteres
+
+    End Function
+
+    Public Function ValidarTelefono()
+        ' Validar que sea numero, de hasta 9 cifras
+
+    End Function
+End Class
---
+++ Practico2/Forms/FormPrincipal.vb
@@ -0,0 +1,22 @@
+﻿Public Class FrmPrincipal
+    Private Sub BtnEnviar_Click(sender As Object, e As EventArgs) Handles BtnEnviar.Click
+        Dim l As New List(Of String)
+        Dim p As New Persona
+
+        For Each line As String In Me.txtTelefonos.Text.Split(vbLf)
+            l.Add(line)
+        Next
+        p.SetTelefono(l)
+
+        MsgBox(p.GetTelefono())
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        Const YES As Integer = 6
+
+        If MsgBox("Desea Salir?", MsgBoxStyle.YesNo) = YES Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
