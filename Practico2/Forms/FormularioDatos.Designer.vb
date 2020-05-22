3f6eacf2af871c1177ac63bf04055a984dfa34a7
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-05-22 13:17:27 -0300

    "Primer Commit Validaciones En Persona y FormPrinicpal"

--- Practico2/Forms/FormularioDatos.Designer.vb
+++ Practico2/Forms/FormularioDatos.Designer.vb
@@ -22,9 +22,16 @@ Partial Class FormularioDatos
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'FormularioDatos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(244, 450)
+        Me.Name = "FormularioDatos"
         Me.Text = "FormularioDatos"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
