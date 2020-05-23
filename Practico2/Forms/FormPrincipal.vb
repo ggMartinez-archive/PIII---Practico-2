8de3517f067e9d09e8ebf40dc187fcc3127f5792
Daniel Lameiro <daniellameiro@outlook.com>
2020-05-23 03:47:01 -0300

    Práctico realizado

--- Practico2/Forms/FormPrincipal.vb
+++ Practico2/Forms/FormPrincipal.vb
@@ -7,7 +7,47 @@
         ' informacion proveniente de este formulario.
 
         Dim p As New Persona
+        Dim formularioDatos As New FrmDatos
 
+        If Char.IsNumber(txtId.Text) Then
+            p.SetId(txtId.Text)
+        End If
+        p.SetNombre(txtNombre.Text)
+        p.SetApellido(txtApellido.Text)
+        p.SetDireccion(txtDireccion.Text)
+        p.SetTelefono(txtTelefonos.Text)
+
+        If p.ValidarID Then
+            formularioDatos.txtIDIngresado.Text = p.GetId().ToString
+        Else
+            formularioDatos.txtIDIngresado.Text = "Error"
+        End If
+
+        If p.ValidaNombre Then
+            formularioDatos.txtNombreIngresado.Text = p.GetNombre
+        Else
+            formularioDatos.txtNombreIngresado.Text = "Error"
+        End If
+
+        If p.ValidarApellido Then
+            formularioDatos.txtApellidoIngresado.Text = p.GetApellido
+        Else
+            formularioDatos.txtApellidoIngresado.Text = "Error"
+        End If
+
+        If p.ValidarDireccion Then
+            formularioDatos.txtDireccionIngresada.Text = p.GetDireccion
+        Else
+            formularioDatos.txtDireccionIngresada.Text = "Error"
+        End If
+
+        If p.ValidarTelefono Then
+            formularioDatos.txtTelefonoIngresado.Text = p.GetTelefono
+        Else
+            formularioDatos.txtTelefonoIngresado.Text = "Error"
+        End If
+
+        formularioDatos.Show()
 
     End Sub
 
